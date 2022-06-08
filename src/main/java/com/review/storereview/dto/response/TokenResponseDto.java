922552360248b337293dfbe928180fc34d0eb760
banan99 <banan99@naver.com>
2022-06-08 18:32:55 +0900

    [REFACTOR] change class name & move

--- src/main/java/com/review/storereview/dto/response/TokenResponseDto.java
+++ src/main/java/com/review/storereview/dto/response/TokenResponseDto.java
@@ -8,13 +8,14 @@ import lombok.*;
  * Author      : 조 준 희
  * Description : JWT토큰 발행 ResponseDTO
  * History     : [2022-01-10] - 조 준희 - Class Create
+ *               [2022-06-01] - 문 윤지 - Class Refactor : 위치 이동, 클래스명 TokenResponseDto로 변경
  */
 @Getter
-public class TokenDto {
+public class TokenResponseDto {
 
     private String token;
 
-    public TokenDto(String token) {
+    public TokenResponseDto(String token) {
         this.token = token;
     }
 }
