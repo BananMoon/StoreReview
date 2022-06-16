55a87fb05676564aea8ac492c1194be737a476ac
banan99 <banan99@naver.com>
2022-06-16 12:40:01 +0900

    [STYLE] 사용하지 않는 import, 코드 제거

--- src/main/java/com/review/storereview/common/AppConfig.java
+++ src/main/java/com/review/storereview/common/AppConfig.java
@@ -1,17 +1,13 @@
 package com.review.storereview.common;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -22,10 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 @RequiredArgsConstructor
-//public class AppConfig {
-public class AppConfig implements WebMvcConfigurer {
-//    private final MultipartHandlerInterceptor multipartHandlerInterceptor;
-
+public class AppConfig {
     @Bean  // 어떤 암호화방식 사용할 것인지 빈 등록
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // 기본은  BCryptPasswordEncoder 방식
@@ -37,10 +30,4 @@ public class AppConfig implements WebMvcConfigurer {
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);   // Java 객체를 JSON으로 Serialize할 때 null값은 제외
         return objectMapper.registerModule(new JavaTimeModule());
     }
-/*
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(multipartHandlerInterceptor)
-                .addPathPatterns("/review")
-                .addPathPatterns("/reviews/**");
-    }*/
 }
