e8c2300798571b0e7653f06e0a00c7abe7ff5477
banan99 <banan99@naver.com>
2022-06-26 16:55:47 +0900

    [REFACTOR] add 'final' keyword to fileds

--- src/main/java/com/review/storereview/dto/response/ReviewFindResponseDto.java
+++ src/main/java/com/review/storereview/dto/response/ReviewFindResponseDto.java
@@ -1,8 +1,6 @@
 package com.review.storereview.dto.response;
 
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 import java.util.List;
 /**
  * {@Summary Review api findOneReview 응답 객체 }
@@ -12,18 +10,17 @@ import java.util.List;
  */
 @Getter
 public class ReviewFindResponseDto {
-
     // List< [REVIEW_ID, SAID, USER_ID, STARS, CONTENT, IMG_URL, CREATED_AT, UPDATED_AT, IS_DELETE]>
-    private Long reviewId;
-    private String said;
-    private String userId;
-    private Integer stars;
-    private String content;
-    private List<String> imgUrl;
-    private String createdAt;
-    private String updatedAt;
-    private Integer isDelete;
-    private int commentNum;
+    private final Long reviewId;
+    private final String said;
+    private final String userId;
+    private final Integer stars;
+    private final String content;
+    private final List<String> imgUrl;
+    private final String createdAt;
+    private final String updatedAt;
+    private final Integer isDelete;
+    private final int commentNum;
 
     // 기본 생성자
     public ReviewFindResponseDto(Long reviewId, String said, String userId, Integer stars, String content, List<String> imgUrl, String createdAt, String updatedAt, Integer isDelete, int commentNum) {
