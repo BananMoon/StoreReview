ce97e626d06be2fb86c7c112a54c32bb9d5f4599
banan99 <banan99@naver.com>
2022-06-26 16:51:53 +0900

    [REFACTOR] add static factory method

--- src/main/java/com/review/storereview/dto/response/ReviewFindListResponseDto.java
+++ src/main/java/com/review/storereview/dto/response/ReviewFindListResponseDto.java
@@ -1,5 +1,8 @@
 package com.review.storereview.dto.response;
 
+import com.review.storereview.common.utils.CryptUtils;
+import com.review.storereview.common.utils.StringUtil;
+import com.review.storereview.dao.cms.Review;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -15,9 +18,13 @@ public class ReviewFindListResponseDto {
     private List<ReviewFindResponseDto> reviewsResponseDtoList;
     private Double placeAvgStar;
 
-    public ReviewFindListResponseDto(Double placeAvgStar) {
+    private ReviewFindListResponseDto(Double placeAvgStar) {
         reviewsResponseDtoList = new ArrayList<>(); // 초기화
         this.placeAvgStar = placeAvgStar;
+    }
+
+    public static ReviewFindListResponseDto createReviewsResponseDto(Double placeAvgStar) {
+        return new ReviewFindListResponseDto(placeAvgStar);
     }
 
     public void addReview(ReviewFindResponseDto reviewDto)
